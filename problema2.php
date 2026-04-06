d0b69b3fa213f9e052d88be6c6737d27d9db0372
Joseph <josephaarongrobles19@gmail.com>
2026-04-06 09:30:00 -0500

    Add files via upload

---
+++ problema2.php
@@ -0,0 +1,44 @@
+<?php
+
+//Operación 1: Suma
+ $numero1 = 5;
+ $numero2 = 3;
+
+ $resultado = $numero1 + $numero2;
+ echo "El resultado de la suma es: $resultado";
+
+ //Operación 2: Resta
+ $numero3 = 10;
+ $numero4 = 4;
+
+ $resultado2 = $numero3 - $numero4;
+echo nl2br ("\nEl resultado de la resta es: $resultado2");
+
+//Operación 3: Multiplicación
+$numero5 = 7;
+$numero6 = 3;
+
+$resultado3 = $numero5 * $numero6;
+echo nl2br("\nEl resultado de la multiplicación es: $resultado3");
+
+//Operación 4: División
+$numero7 = 15;
+$numero8 = 5;
+
+$resultado4 = $numero7 / $numero8;
+echo nl2br("\nEl resultado de la división es: $resultado4");
+
+//Operación 5: Módulo
+$numero9 = 10;
+$numero10 = 3;
+
+$resultado5 = $numero9 / $numero10;
+echo nl2br("\nEl resultado de módulo da: $resultado5");
+
+//Operación 6: Potencia
+$base = 2;
+$exponente = 3;
+
+$resultadoM = $base ** $exponente;
+echo nl2br("\nEl resultado de $base al exponente $exponente es $resultadoM");
+?>
---
+++ problema3.php
@@ -0,0 +1,13 @@
+<?php
+
+//Operaciones de Orden de Procedencia
+
+//Ejemplo 1
+$resultado1 = 2 + 3 * 4;
+echo nl2br("El resultado del ejemplo 1 es: $resultado1");
+
+//Ejemplo 2
+$resultado2 = (2 + 3) * 4;
+echo nl2br("\nEl resultado del ejemplo 2 es: $resultado2");
+
+?>
